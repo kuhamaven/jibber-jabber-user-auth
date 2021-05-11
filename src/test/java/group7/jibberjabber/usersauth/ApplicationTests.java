adb85d7ee31ef03bfff2b39094b4962248f12ade
kuhamaven <khalil.stessens@ing.austral.edu.ar>
2021-05-11 12:09:45 -0300

    Spring initializr

---
+++ src/test/java/group7/jibberjabber/usersauth/ApplicationTests.java
@@ -0,0 +1,13 @@
+package group7.jibberjabber.usersauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
